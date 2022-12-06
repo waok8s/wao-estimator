0f78da8953b1cf8ad4cb7bbf66ee5cafb35a1fa1
Shunsuke Ise <ise@bitmedia.co.jp>
2022-12-06 16:12:39 +0900

    CI

---
+++ hack/dist-manifests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# scripts must be run from project root
+. hack/0-env.sh || exit 1
+
+# consts
+
+VERSION=$(git describe --tags --match "v*")
+IMG=$PROJECT_NAME-controller:$VERSION
+REGISTRY=${REGISTRY:-""}
+if [ -n "$REGISTRY" ]; then
+    IMG=$REGISTRY/$PROJECT_NAME-controller:$VERSION
+fi
+DIST_FILE=wao-estimator.yaml
+
+make kustomize
+
+cd config/manager && "$PROJECT_ROOT/bin/kustomize" edit set image controller="$IMG"
+cd "$PROJECT_ROOT" && "$PROJECT_ROOT/bin/kustomize" build config/default > $DIST_FILE
