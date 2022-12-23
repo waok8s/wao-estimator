cd583838083bfaa638f55ce159f291339cf6a020
Shunsuke Ise <ise@bitmedia.co.jp>
2022-12-23 17:08:46 +0900

    MLServerPCPredictor

--- pkg/estimator/predictor_pc.go
+++ pkg/estimator/predictor_pc.go
@@ -1,8 +1,14 @@
 package estimator
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
+
+	http2curl "moul.io/http2curl/v2"
 )
 
 type PowerConsumptionPredictor interface {
@@ -37,12 +43,153 @@ func PredictPCFnDummy(_ context.Context, mcpu int, status NodeStatus) (float64, 
 }
 
 type MLServerPCPredictor struct {
-	// TODO
+	// Server specifies server address
+	// e.g. "http://localhost:8080"
+	Server string
+	// Model specifies model name
+	// e.g. "model1"
+	Model string
+	// Version specifies version for the model
+	// e.g. "v0.1.0"
+	Version string
 }
 
 var _ PowerConsumptionPredictor = (*MLServerPCPredictor)(nil)
 
 func (p *MLServerPCPredictor) Predict(ctx context.Context, requestCPUMilli int, status NodeStatus) (watt float64, err error) {
-	// TODO
-	return 0.0, fmt.Errorf("not yet implemented (%w)", ErrPCPredictor)
+
+	// TODO: convert requestCPUMilli to CPU usage
+	cpuUsage := 10.0
+	// TODO: fetch the ambient sensor number used in the model and get the value
+	ambientTemp := 22.0
+	// TODO: fetch staticPressureDiff
+	staticPressureDiff := 0.2
+
+	return p.POSTPredictRequest(ctx, cpuUsage, ambientTemp, staticPressureDiff)
+}
+
+// getURLV2Infer returns the API endpoint.
+// e.g. "http://localhost:8080/v2/models/model1/versions/v0.1.0/infer"
+func (p *MLServerPCPredictor) getURLV2Infer() (string, error) {
+	return url.JoinPath(p.Server, "v2", "models", p.Model, "versions", p.Version, "infer")
+}
+
+func (p *MLServerPCPredictor) POSTPredictRequest(ctx context.Context, cpuUsage, ambientTemp, staticPressureDiff float64) (float64, error) {
+
+	url, err := p.getURLV2Infer()
+	if err != nil {
+		return 0.0, fmt.Errorf("unable to get endpoint URL: %w", err)
+	}
+
+	body, err := json.Marshal(newMLServerPCPredictorRequest(cpuUsage, ambientTemp, staticPressureDiff))
+	if err != nil {
+		return 0.0, fmt.Errorf("unable to marshal the request body=%+v err=%w", body, err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return 0.0, fmt.Errorf("unable to create HTTP request: %w", err)
+	}
+
+	curl, err := http2curl.GetCurlCommand(req)
+	if err != nil {
+		lg.Err(err).Msgf("MLServerPCPredictor.Predict could not parse http.Request to curl command")
+	} else {
+		lg.Trace().Msgf("MLServerPCPredictor.Predict request=%v", curl.String())
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0.0, fmt.Errorf("unable to send HTTP request: %w", err)
+	}
+	switch resp.StatusCode {
+	case http.StatusOK:
+		var predictResp mlServerPCPredictorResponse
+		if err := json.NewDecoder(resp.Body).Decode(&predictResp); err != nil {
+			return 0.0, fmt.Errorf("could not decode resp: %w", err)
+		}
+		if len(predictResp.Outputs) == 0 || len(predictResp.Outputs[0].Data) == 0 {
+			return 0.0, fmt.Errorf("invalid response predictResp=%+v", predictResp)
+		}
+		return predictResp.Outputs[0].Data[0], nil
+	default:
+		return 0.0, fmt.Errorf("HTTP status=%v request=%v", resp.Status, curl.String())
+	}
+
+}
+
+// mlServerPCPredictorRequest holds a request.
+//
+// e.g.
+//
+//	{
+//	  "inputs": [
+//	    {
+//	      "name": "predict-prob",
+//	      "shape": [ 1, 3 ],
+//	      "datatype": "FP32",
+//	      "data": [ [10, 22, 0.2 ] ]
+//	    }
+//	  ]
+//	}
+type mlServerPCPredictorRequest struct {
+	Inputs []struct {
+		Name     string      `json:"name"`
+		Shape    []int       `json:"shape"`
+		Datatype string      `json:"datatype"`
+		Data     [][]float32 `json:"data"`
+	} `json:"inputs"`
+}
+
+func newMLServerPCPredictorRequest(cpuUsage, ambientTemp, staticPressureDiff float64) *mlServerPCPredictorRequest {
+	const (
+		name     = "predict-prob"
+		datatype = "FP32"
+		shapeX   = 1
+		shapeY   = 3
+	)
+	return &mlServerPCPredictorRequest{
+		Inputs: []struct {
+			Name     string      `json:"name"`
+			Shape    []int       `json:"shape"`
+			Datatype string      `json:"datatype"`
+			Data     [][]float32 `json:"data"`
+		}{
+			{
+				Name:     name,
+				Shape:    []int{shapeX, shapeY},
+				Datatype: datatype,
+				Data:     [][]float32{{float32(cpuUsage), float32(ambientTemp), float32(staticPressureDiff)}},
+			},
+		},
+	}
+}
+
+// mlServerPCPredictorResponse holds a response.
+// Ignore values except outputs[*].data[]
+//
+// e.g.
+//
+//	{
+//	  "model_name": "model1",
+//	  "model_version": "v0.1.0",
+//	  "id": "0dc429d2-bd02-404b-b624-a0fa628e451e",
+//	  "parameters": {
+//	    "content_type": null,
+//	    "headers": null
+//	  },
+//	  "outputs": [
+//	    {
+//	      "name": "predict",
+//	      "shape": [ 1, 1 ],
+//	      "datatype": "FP64",
+//	      "parameters": null,
+//	      "data": [ 94.76267448501928 ]
+//	    }
+//	  ]
+//	}
+type mlServerPCPredictorResponse struct {
+	Outputs []struct {
+		Data []float64 `json:"data"`
+	} `json:"outputs"`
 }
--- pkg/estimator/predictor_pc_test.go
+++ pkg/estimator/predictor_pc_test.go
@@ -2,6 +2,8 @@ package estimator
 
 import (
 	"context"
+	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -47,6 +49,115 @@ func TestFakePCPredictor_Predict(t *testing.T) {
 			}
 			if gotWatt != tt.wantWatt {
 				t.Errorf("FakePCPredictor.Predict() = %v, want %v", gotWatt, tt.wantWatt)
+			}
+		})
+	}
+}
+
+func Test_newMLServerPCPredictorRequest(t *testing.T) {
+	type args struct {
+		cpuUsage           float64
+		ambientTemp        float64
+		staticPressureDiff float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"10/22/0.2",
+			args{cpuUsage: 10.0, ambientTemp: 22.0, staticPressureDiff: 0.2},
+			`{"inputs":[{"name":"predict-prob","shape":[1,3],"datatype":"FP32","data":[[10,22,0.2]]}]}`,
+		},
+		{"99.9/99.9/99.9",
+			args{cpuUsage: 99.9, ambientTemp: 99.9, staticPressureDiff: 99.9},
+			`{"inputs":[{"name":"predict-prob","shape":[1,3],"datatype":"FP32","data":[[99.9,99.9,99.9]]}]}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := newMLServerPCPredictorRequest(tt.args.cpuUsage, tt.args.ambientTemp, tt.args.staticPressureDiff)
+			p, err := json.Marshal(v)
+			if err != nil {
+				t.Errorf("unable to encode err=%v", err)
+			}
+			if !reflect.DeepEqual(p, []byte(tt.want)) {
+				t.Errorf("newMLServerPCPredictorRequest() = %s, want %s", p, tt.want)
+			}
+		})
+	}
+}
+
+func Test_decodeJSON_mlServerPCPredictorResponse(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  mlServerPCPredictorResponse
+	}{
+		{"94.76267448501928",
+			`{"model_name":"model1","model_version":"v0.1.0","id":"c0d61974-f9bd-48ac-9af8-9c1e6d9a07e6","parameters":{"content_type":null,"headers":null},"outputs":[{"name":"predict","shape":[1,1],"datatype":"FP64","parameters":null,"data":[94.76267448501928]}]}`,
+			mlServerPCPredictorResponse{Outputs: []struct {
+				Data []float64 `json:"data"`
+			}{{Data: []float64{94.76267448501928}}},
+			},
+		},
+		{"15",
+			`{"model_name":"model1","model_version":"v0.1.0","id":"c0d61974-f9bd-48ac-9af8-9c1e6d9a07e6","parameters":{"content_type":null,"headers":null},"outputs":[{"name":"predict","shape":[1,1],"datatype":"FP64","parameters":null,"data":[15]}]}`,
+			mlServerPCPredictorResponse{Outputs: []struct {
+				Data []float64 `json:"data"`
+			}{{Data: []float64{15}}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got mlServerPCPredictorResponse
+			if err := json.Unmarshal([]byte(tt.input), &got); err != nil {
+				t.Errorf("unable to decode err=%v", err)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mlServerPCPredictorResponse got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMLServerPCPredictor_getURLV2Infer(t *testing.T) {
+	type fields struct {
+		Server  string
+		Model   string
+		Version string
+	}
+	tests := []struct {
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{fields{
+			Server:  "http://localhost:8080",
+			Model:   "model1",
+			Version: "v0.1.0",
+		}, "http://localhost:8080/v2/models/model1/versions/v0.1.0/infer", false},
+		{fields{
+			Server:  "https://example.com",
+			Model:   "model2",
+			Version: "v1.0.0",
+		}, "https://example.com/v2/models/model2/versions/v1.0.0/infer", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.want, func(t *testing.T) {
+			p := &MLServerPCPredictor{
+				Server:  tt.fields.Server,
+				Model:   tt.fields.Model,
+				Version: tt.fields.Version,
+			}
+			got, err := p.getURLV2Infer()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MLServerPCPredictor.getURLV2Infer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("MLServerPCPredictor.getURLV2Infer() = %v, want %v", got, tt.want)
 			}
 		})
 	}
